e29a4c9d812244fedb17c90dbb775c6d051d2b70
Artur_Miarczynski <artur_miarczynski@epam.com>
2020-08-20 09:50:35 +0200

    bugfixes

--- src/app.js
+++ src/app.js
@@ -6,17 +6,23 @@ const APPLICATION_PORT = 8080;
 const CONFLICT = 409;
 const NOT_FOUND = 404;
 const CREATED = 201;
+const NO_CONTENT = 204;
 
 app.use(bodyParser.json())
 
 let users = [
-    { id: 1, name: 'John', secondName: 'Kowalski', role: 'admin' },
-    { id: 2, name: 'Jane', secondName: 'Nowak', role: 'moderator' },
-    { id: 3, name: 'Steven', secondName: 'Seagal', role: 'user' }
+    { id: 1, name: 'John', secondName: 'Kowalski', role: 'admin', deleted: false },
+    { id: 2, name: 'Jane', secondName: 'Nowak', role: 'moderator', deleted: false },
+    { id: 3, name: 'Steven', secondName: 'Seagal', role: 'user', deleted: false }
 ];
 
+function getNotDeletedUsers()
+{
+    return users.filter(user => !user.deleted);
+}
+
 app.get('/users/:id', (req, res) => {
-    const user = users.find(user => user.id == req.params.id);
+    const user = getNotDeletedUsers().find(user => user.id == req.params.id);
 
     if (user)
         res.json(user);
@@ -25,22 +31,22 @@ app.get('/users/:id', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-    res.json(users);
+    res.json(getNotDeletedUsers());
 });
 
 app.post('/users', (req, res) => {
-    const user = users.find(user => user.id == req.body.id);
+    const user = getNotDeletedUsers().find(user => user.id == req.body.id);
     if (user) {
         res.status(CONFLICT).json('User already exists');
         return;
     }
 
     users.push(req.body);
-    res.json(req.body);
+    res.status(CREATED).json(req.body);
 });
 
 app.put('/users', (req, res) => {
-    const userIndex = users.map(user => user.id).indexOf(req.body.id);
+    const userIndex = getNotDeletedUsers().map(user => user.id).indexOf(req.body.id);
     if (userIndex < 0) {
         res.status(NOT_FOUND).json('User doesnt exist');
         return;
@@ -51,11 +57,15 @@ app.put('/users', (req, res) => {
 });
 
 app.delete('/users/:id', (req, res) => {
-    const user = users.find(user => user.id == req.params.id);
+    const user = getNotDeletedUsers().find(user => user.id == req.params.id);
     if (user) {
         const userIndex = users.indexOf(user);
-        users.splice(userIndex, 1);
-        res.status(CREATED).json({ message: "OK" });
+        users[userIndex].deleted = true;
+        res.status(NO_CONTENT).json('ok');
+    }
+    else
+    {
+        res.status(NOT_FOUND).json('User doesnt exist');
     }
 });
 
@@ -66,7 +76,7 @@ app.get('/users/suggest/:nameFilter', (req, res) => {
         limit = req.query.limit;
     }
 
-    const filteredUsers = users
+    const filteredUsers = getNotDeletedUsers()
         .filter(user => user.name.startsWith(req.params.nameFilter))
         .slice(0, req.query.limit);
     res.json(filteredUsers);
